f56c63d4facbacfedfd1b08f33996e2e3f19995f
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-12 17:36:21 -0400

    Add prepend and size

--- list.js
+++ list.js
@@ -1,12 +1,24 @@
 class LinkedList {
     constructor() {
     this.head = null;
+    this.size = 0;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value, this.head);
+        this.head = newNode;
+        this.size++;
     }
 }
 
 class Node {
-    constructor() {
-      this.value = null;
-      this.nextNode = null;
+    constructor(value, nextNode) {
+      this.value = value;
+      this.next = nextNode;
     }
 }
+
+const testList = new LinkedList();
+testList.prepend(10);
+testList.prepend(20);
+console.log(testList);
